043a99598cf5316281f0f5dfc242043532d39c51
dhimassena <dhimassr@gmail.com>
2025-05-11 11:30:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,60 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+# -----------------------------------------------------------------------------
+# Draw the actual page
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Set the title that appears at the top of the page.
+"""
+# :sparkles: Proyek Jaya Jaya Institut (Prototype)
+
+**Dicoding IDCamp 2025**
+
+"""
+
+st.info(
+    """
+    Nama: [Dhimas Sena Rahmantara]\n
+    Email: [dhimassr@gmail.com]\n
+    ID Dicoding: [dhimassena]
+    """
 )
+
+data = pd.DataFrame()
+
+previous_qualification_grade = int(st.number_input(label='previous_qualification_grade', value=120))
+data["previous_qualification_grade"] = previous_qualification_grade
+
+admission_grade = int(st.number_input(label='admission_grade', value=150))
+data["admission_grade"] = admission_grade
+
+age_at_enrollment = int(st.number_input(label='age_at_enrollment', value=28))
+data["age_at_enrollment"] = age_at_enrollment
+
+curricular_units_1st_sem_evaluations = int(st.number_input(label='curricular_units_1st_sem_evaluations', value=10))
+data["curricular_units_1st_sem_evaluations"] = curricular_units_1st_sem_evaluations
+
+curricular_units_1st_sem_approved = int(st.number_input(label='curricular_units_1st_sem_approved', value=5))
+data["curricular_units_1st_sem_approved"] = curricular_units_1st_sem_approved
+
+curricular_units_1st_sem_grade = int(st.number_input(label='curricular_units_1st_sem_grade', value=6))
+data["curricular_units_1st_sem_grade"] = curricular_units_1st_sem_grade
+
+curricular_units_2nd_sem_evaluations = int(st.number_input(label='curricular_units_2nd_sem_evaluations', value=4))
+data["curricular_units_2nd_sem_evaluations"] = curricular_units_2nd_sem_evaluations
+
+curricular_units_2nd_sem_approved = int(st.number_input(label='curricular_units_2nd_sem_approved', value=8))
+data["curricular_units_2nd_sem_approved"] = curricular_units_2nd_sem_approved
+
+curricular_units_2nd_sem_grade = int(st.number_input(label='curricular_units_2nd_sem_grade', value=9))
+data["curricular_units_2nd_sem_grade"] = curricular_units_2nd_sem_grade
+
+gdp = int(st.number_input(label='gdp', value=5))
+data["gdp"] = curricular_units_2nd_sem_grade
+
+if st.button('Predict'):
+    new_data = preprocessing(data=data)
+    with st.expander("View the Preprocessed Data"):
+        st.dataframe(data=new_data, width=800, height=10)
+    st.write("Hasil Prediksi: {}".format(prediction(new_data)))
