812c46fc36ac362d8a2ea96679971e22089e8e40
dhimassena <dhimassr@gmail.com>
2025-05-11 22:56:55 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -10,7 +10,7 @@ def data_prep(df):
     # df = pd.DataFrame()
     # df = scaler.transform(df)
 
-    df["previous_qualification_grade"] = scaler.transform(df[["previous_qualification_grade"]["admission_grade"]])
+    df["previous_qualification_grade"] = scaler.transform(df[["previous_qualification_grade"]].iloc[0])
     df["admission_grade"] = scaler.transform(df[["admission_grade"]])
     df["age_at_enrollment"] = scaler.transform(df[["age_at_enrollment"]])
     df["curricular_units_1st_sem_evaluations"] = scaler.transform(df[["curricular_units_1st_sem_evaluations"]])
