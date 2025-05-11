98abde3f64454b251820106ad894a7033d732a0f
dhimassena <dhimassr@gmail.com>
2025-05-11 22:30:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ with st.expander("View the Raw Data"):
     st.dataframe(data=data, width=800, height=10)
 
 if st.button('Prediksi'):
-    st.info(data)
+    st.info(data['gdp'])
     new_data = data_prep(data=data)
     with st.expander("View the Preprocessed Data"):
         st.dataframe(data=new_data, width=800, height=10)
