3d92d90dd9897b662f9f1ea19af133be07d1a51b
dhimassena <dhimassr@gmail.com>
2025-05-12 10:03:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.info(
     ID Dicoding: [dhimassena]
     """
 )
-
 # data = pd.DataFrame()
+
 col1, col2 = st.columns(2)
 with col1:
     previous_qualification_grade = int(st.number_input(label='previous_qualification_grade', value=120))
@@ -31,29 +31,37 @@ with col2:
     admission_grade = int(st.number_input(label='admission_grade', value=150))
     # data["admission_grade"] = admission_grade
 
-age_at_enrollment = int(st.number_input(label='age_at_enrollment', value=28))
-# data["age_at_enrollment"] = age_at_enrollment
+col1, col2 = st.columns(2)
+with col1:
+    age_at_enrollment = int(st.number_input(label='age_at_enrollment', value=28))
+    # data["age_at_enrollment"] = age_at_enrollment
+with col2:
+    curricular_units_1st_sem_evaluations = int(st.number_input(label='curricular_units_1st_sem_evaluations', value=10))
+    # data["curricular_units_1st_sem_evaluations"] = curricular_units_1st_sem_evaluations
 
-curricular_units_1st_sem_evaluations = int(st.number_input(label='curricular_units_1st_sem_evaluations', value=10))
-# data["curricular_units_1st_sem_evaluations"] = curricular_units_1st_sem_evaluations
+col1, col2 = st.columns(2)
+with col1:
+    curricular_units_1st_sem_approved = int(st.number_input(label='curricular_units_1st_sem_approved', value=5))
+    # data["curricular_units_1st_sem_approved"] = curricular_units_1st_sem_approved
+with col2:
+    curricular_units_1st_sem_grade = int(st.number_input(label='curricular_units_1st_sem_grade', value=6))
+    # data["curricular_units_1st_sem_grade"] = curricular_units_1st_sem_grade
 
-curricular_units_1st_sem_approved = int(st.number_input(label='curricular_units_1st_sem_approved', value=5))
-# data["curricular_units_1st_sem_approved"] = curricular_units_1st_sem_approved
+col1, col2 = st.columns(2)
+with col1:
+    curricular_units_2nd_sem_evaluations = int(st.number_input(label='curricular_units_2nd_sem_evaluations', value=4))
+    # data["curricular_units_2nd_sem_evaluations"] = curricular_units_2nd_sem_evaluations
+with col2:
+    curricular_units_2nd_sem_approved = int(st.number_input(label='curricular_units_2nd_sem_approved', value=8))
+    # data["curricular_units_2nd_sem_approved"] = curricular_units_2nd_sem_approved
 
-curricular_units_1st_sem_grade = int(st.number_input(label='curricular_units_1st_sem_grade', value=6))
-# data["curricular_units_1st_sem_grade"] = curricular_units_1st_sem_grade
-
-curricular_units_2nd_sem_evaluations = int(st.number_input(label='curricular_units_2nd_sem_evaluations', value=4))
-# data["curricular_units_2nd_sem_evaluations"] = curricular_units_2nd_sem_evaluations
-
-curricular_units_2nd_sem_approved = int(st.number_input(label='curricular_units_2nd_sem_approved', value=8))
-# data["curricular_units_2nd_sem_approved"] = curricular_units_2nd_sem_approved
-
-curricular_units_2nd_sem_grade = int(st.number_input(label='curricular_units_2nd_sem_grade', value=9))
-# data["curricular_units_2nd_sem_grade"] = curricular_units_2nd_sem_grade
-
-gdp = int(st.number_input(label='gdp', value=5))
-# data["gdp"] = gdp
+col1, col2 = st.columns(2)
+with col1:
+    curricular_units_2nd_sem_grade = int(st.number_input(label='curricular_units_2nd_sem_grade', value=9))
+    # data["curricular_units_2nd_sem_grade"] = curricular_units_2nd_sem_grade
+with col2:
+    gdp = int(st.number_input(label='gdp', value=5))
+    # data["gdp"] = gdp
 
 data = {
     "previous_qualification_grade": [previous_qualification_grade],
